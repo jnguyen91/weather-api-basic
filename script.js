71d7cbf2cf74e1f142fcb8cb7156e4fcacac12b7
Jeff Nguyen <jeffnguyen91@hotmail.com>
2020-08-07 16:39:48 -0700

    screenshot added and additional code

--- script.js
+++ script.js
@@ -1,6 +1,24 @@
+var city = "Orlando";
+
 $.getJSON(
-	"http://api.openweathermap.org/data/2.5/weather?q=Vancouver&APPID=35defc8175fa6a216fb0087a787",
+	"http://api.openweathermap.org/data/2.5/weather?q=" + city + "&APPID=35defc8175fa6a216fb0087a7875e9a4",
 	function(data){
-		console.log(data);
+        console.log(data);
+        
+        var icon = "http://api.openweathermap.org/img/w/" + data.weather[0].icon + ".png";
+
+        var temp = Math.floor(data.main.temp);
+        var weather = data.weather[0].main;
+        var name = data.main.name;
+        var country = data.sys.country;
+
+        
+        $(".country").append(country);
+        $(".name").append(city);
+        
+        $(".icon").attr("src", icon);
+        $(".temp").append(temp); 
+        $(".weather").append(weather);
+        
 	}
 );
